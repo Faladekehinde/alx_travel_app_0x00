b06a2cf019ea8b2120225f645f38bc528257a477
Faladekehinde <dafaladexy@gmail.com>
2025-06-07 14:11:30 +0100

    Adding all files

--- alx_travel_app/alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/alx_travel_app/urls.py
@@ -33,6 +33,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$', lambda request: redirect('swagger/', permission=False)),
+    re_path(r'^$', lambda request: redirect('swagger/', permanent=False)),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
